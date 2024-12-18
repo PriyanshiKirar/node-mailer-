c93de3ece061f94fa5d4a5d69b4da1808e782977
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-12-18 23:06:42 +0530

    18Dec

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const sendEmail = require('./email'); // Declare once
+
+// Route to send email
+app.get('/send-email', async (req, res) => {
+  try {
+    await sendEmail(
+      'asthap602@gmail.com',
+      'Test Email Subject',
+      'This is a test email sent with Nodemailer using OAuth2.',
+      '<p>This is a test email sent with <b>Nodemailer</b> using OAuth2.</p>'
+    );
+    res.send('Email sent successfully!');
+  } catch (error) {
+    res.status(500).send('Error sending email: ' + error.message);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000...");
+});
